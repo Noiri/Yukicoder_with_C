a241e0c6431194f0a369de1d805d8575b3002c4c
Noiri <suleipenn@gmail.com>
2019-03-04 18:22:54 +0900

    solved

---
+++ level_1.5/No769_UNOシミュレータ.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n, m;
+    cin >> n >> m;
+    vector<int> ans(n);
+    vector<string> v(m);
+    for(int i=0; i<m; i++) cin >> v[i];
+
+
+    
+}
